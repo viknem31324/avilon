b161c82fa89a3d19b420fb9b44f62fa7597051f5
Alena Safyannikova <a.safyannikova@kokoc.tech>
2022-11-23 17:35:03 +0500

    chore: init repo

---
+++ app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+  storybook: {
+    // Options
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+});
