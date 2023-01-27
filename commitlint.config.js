d0625a71d3988643aee17a542269400367f2048f
Алена Сафьянникова <a.safyannikova@kokoc.tech>
2023-01-27 15:40:29 +0300

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-        'feature',
-        'hotfix',
-        'bugfix',
-      ],
-    ],
-  },
 };
