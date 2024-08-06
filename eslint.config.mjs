a36cb59be99bd77585e46082c6e3a08adeb437a6
Максим Тутуков <m.tutukov@kokoc.tech>
2024-08-06 17:53:30 +0300

    feat(master): дорабатывает сборку

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,6 +27,14 @@ export default withNuxt({
         },
         multilineDetection: 'brackets',
       }],
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: { void: 'always', normal: 'always', component: 'always' },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
       'vue/max-attributes-per-line': ['error', { singleline: { max: 3 }, multiline: { max: 1 } }],
     },
   });
