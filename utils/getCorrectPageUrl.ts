57ee0e537f6e70020cc63c98714cf5b4e1311750
Максим Тутуков <m.tutukov@mail.ru>
2025-10-23 11:20:13 +0300

    fix(master): исправляет getCorrectPageUrl

--- utils/getCorrectPageUrl.ts
+++ utils/getCorrectPageUrl.ts
@@ -3,7 +3,7 @@ const allowedExtensions = ['svg', 'png', 'jpg', 'jpeg'];
 /**
  * удаляет дубли слешей, добавляет слеш в конце
  * @param path
- */
+*/
 export const getCorrectPageUrl = (path?: string | null): string => {
   if (!path) {
     return '';
@@ -14,7 +14,13 @@ export const getCorrectPageUrl = (path?: string | null): string => {
     || allowedExtensions.includes(path.split('.').pop() || '')
     || path[0] === '#'
   ) {
-    return path;
+    let cleanedPath = path.replace(/([^:]\/)\/+/g, '$1');
+
+    if (!cleanedPath.endsWith('/') && !cleanedPath.includes('#') && !cleanedPath.includes('?') && !cleanedPath.includes('.')) {
+      cleanedPath += '/';
+    }
+
+    return cleanedPath;
   }
 
   return `/${path}/`.replace(/\/\/+/g, '/');
